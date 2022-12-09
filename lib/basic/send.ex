9fd49bee9f92591570327a20f29b7b7c81e95563
Merlin Webster <merlin.webster@multiverse.io>
2022-12-09 10:03:25 +0000

    Fixed Basic.send

--- lib/basic/send.ex
+++ lib/basic/send.ex
@@ -1,9 +1,9 @@
 defmodule Basic.Send do
-  def send(channel, message) do
+  def send(message, queue_name, exchange_name \\ "", publish_ops \\ []) do
     with {:ok, connection} <- AMQP.Connection.open(),
          {:ok, channel} <- AMQP.Channel.open(connection),
-         {:ok, _queue} <- AMQP.Queue.declare(channel, "hello"),
-         :ok <- AMQP.Basic.publish(channel, "", "hello", message),
+         {:ok, _queue} <- AMQP.Queue.declare(channel, queue_name),
+         :ok <- AMQP.Basic.publish(channel, exchange_name, queue_name, message, publish_ops),
          :ok <- AMQP.Connection.close(connection),
          do: :ok
   end
